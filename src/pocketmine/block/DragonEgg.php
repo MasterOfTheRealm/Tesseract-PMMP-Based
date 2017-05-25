20d0529a761bc9113972c4865bc5ba25172691ba
poustmal <poustmal12@gmail.com>
2017-05-25 23:00:21 +0300

    A bit more functionable draggon egg from clearsky

--- src/pocketmine/block/DragonEgg.php
+++ src/pocketmine/block/DragonEgg.php
@@ -2,49 +2,47 @@
 
 /*
  *
- *    _______                                _
- *   |__   __|                              | |
- *      | | ___  ___ ___  ___ _ __ __ _  ___| |_
- *      | |/ _ \/ __/ __|/ _ \  __/ _` |/ __| __|
- *      | |  __/\__ \__ \  __/ | | (_| | (__| |_
- *      |_|\___||___/___/\___|_|  \__,_|\___|\__|
- *
+ *  ____            _        _   __  __ _                  __  __ ____
+ * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
+ * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
+ * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
+ * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * @author Tessetact Team
- * @link http://www.github.com/TesseractTeam/Tesseract
+ * @author PocketMine Team
+ * @link http://www.pocketmine.net/
  *
  *
- */
+*/
 
 namespace pocketmine\block;
 
-use pocketmine\item\Item;
+use pocketmine\item\Tool;
 
-class DragonEgg extends Solid{
+class DragonEgg extends Fallable{
 
 	protected $id = self::DRAGON_EGG;
 
-	public function __construct(){
-	}
+	public function __construct(){}
+
+    public function getHardness(){
+        return 3;
+    }
+
+    public function getResistance(){
+        return 45;
+    }
+
+    public function getLightLevel(){
+        return 1;
+    }
 
 	public function getName(){
 		return "Dragon Egg";
 	}
 
-	public function getHardness(){
-		return -1;
-	}
-	
-	public function getResistance(){
-		return 18000000;
-	}
-
-	public function isBreakable(Item $item){
-		return false;
-	}
 }
