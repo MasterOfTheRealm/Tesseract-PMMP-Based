998fcb8eb41bba0bee3e8e15a1094cf84f8c910e
FunnyBuddys <saklisamuela@gmail.com>
2017-06-03 05:05:12 +0200

    Oops

--- src/pocketmine/inventory/AnvilInventory.php
+++ src/pocketmine/inventory/AnvilInventory.php
@@ -23,60 +23,6 @@ namespace pocketmine\inventory;
 
 use pocketmine\item\Item;
 use pocketmine\item\EnchantedBook;
-use pocketmine\level\Position;
-use pocketmine\Player;
-use pocketmine\Server;
-use pocketmine\event\inventory\AnvilProcessEvent;
-
-class AnvilInventory extends TemporaryInventory {
-
-    const TARGET = 0;
-    const SACRIFICE = 1;
-    const RESULT = 2;
-
-
-    public function __construct(Position $pos) {
-        parent::__construct(new FakeBlockMenu($this, $pos), InventoryType::get(InventoryType::ANVIL));
-    }
-
-    /**
-     * @return FakeBlockMenu|InventoryHolder
-     */
-    public function getHolder() {
-        return $this->holder;
-    }
-
-    public function getResultSlotIndex() {
-        return self::RESULT;
-    }
-
-    public function onRename(Player $player, Item $resultItem): bool {
-        if (!$resultItem->equals($this->getItem(self::TARGET), true, false, true)) {
-<?php
-
-/*
- *
- *  ____            _        _   __  __ _                  __  __ ____
- * |  _ \ ___   ___| | _____| |_|  \/  (_)_ __   ___      |  \/  |  _ \
- * | |_) / _ \ / __| |/ / _ \ __| |\/| | | '_ \ / _ \_____| |\/| | |_) |
- * |  __/ (_) | (__|   <  __/ |_| |  | | | | | |  __/_____| |  | |  __/
- * |_|   \___/ \___|_|\_\___|\__|_|  |_|_|_| |_|\___|     |_|  |_|_|
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * @author PocketMine Team
- * @link http://www.pocketmine.net/
- *
- *
-*/
-
-namespace pocketmine\inventory;
-
-use pocketmine\item\Item;
-use pocketmine\item\EnchantedBook;
 use pocketmine\item\enchantment\Enchantment;
 use pocketmine\level\Position;
 use pocketmine\Player;
@@ -175,3 +121,4 @@ class AnvilInventory extends TemporaryInventory{
 	}
 
 }
+ 
