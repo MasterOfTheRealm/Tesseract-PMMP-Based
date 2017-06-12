007b1cd651953596422402c0e47ba6b5d1f2da7e
Realm <Kavindujayasinghe2000@Gmail.com>
2017-06-12 21:36:02 +0530

    Proper Zombie Drops

--- src/pocketmine/entity/Zombie.php
+++ src/pocketmine/entity/Zombie.php
@@ -57,7 +57,7 @@ class Zombie extends Monster{
 
 	public function getDrops(){
 		$drops = [
-			ItemItem::get(ItemItem::FEATHER, 0, 1)
+			ItemItem::get(ItemItem::ROTTEN_FLESH, 0, mt_rand(0, 2))
 		];
 		if($this->lastDamageCause instanceof EntityDamageByEntityEvent and $this->lastDamageCause->getEntity() instanceof Player){
 			if(mt_rand(0, 199) < 5){
